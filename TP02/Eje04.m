fe310de666e0330c5bdbfa8e3997b898e1c9ac54
Luciano Perezzini at Francisco <lperezzini@gmail.com>
2016-11-16 16:49:56 -0300

    TP02/Eje04.m: display normalized histograms

--- TP02/Eje04.m
+++ TP02/Eje04.m
@@ -1,7 +1,7 @@
 % Eje04: ecualización de imágenes
 % Inicialización
 clear all
-close all
+%close all
 
 Resp = input('Nombre imagen: ', 's');
 
@@ -13,13 +13,13 @@ I = imread(Resp);
 %%
 J = ecualizar(I);
 
-% hi = histograma(I);
-% hj = histograma(J);
+hi = histograma(I);
+hj = histograma(J);
 %%
 figure, imshow([I J])
 
-figure, subplot(211), imhist(I), title('Original')
-subplot(212), imhist(J), title('Procesada')
+figure, subplot(211), plot(0:255, hi), title('Original'), grid on
+subplot(212),plot(0:255, hj), title('Procesada'), grid on
 
 % imhist() no devuelve un histograma normalizado. Para ésto podemos usar, 
 % por ejemplo, plot(0:255, hi)
