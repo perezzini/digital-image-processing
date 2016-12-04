a0aca14ecb636b20dac9188f436ac6c48061b61d
Luciano Perezzini at Francisco <lperezzini@gmail.com>
2016-12-04 13:15:41 -0300

    TP04/Ejer02a.m: show 1D DFT spectrum

--- TP04/Ejer02a.m
+++ TP04/Ejer02a.m
@@ -4,16 +4,30 @@ clear all
 N = 256;
 f0 = 5;
 t = (0:N-1)/N;
+Dt = t(2)-t(1);
 
-x = mat2gray(cos(2*pi*f0*t));
+Du = 1/Dt/N; % discretización de frecuencias
+if rem(N, 2) ~= 0 % N es impar
+    u = (-(N-1)/2:(N-1)/2)*Du;
+else
+    u = (-N/2:N/2-1)*Du;
+end
 
-figure, plot(t, x), grid on
+x1D = cos(2*pi*f0*t);
+x2D = mat2gray(cos(2*pi*f0*t));
 
-I = repmat(x, size(x'));
+figure, plot(t, x2D), grid on, title('Señal')
 
-figure, imshow(I)
+I = repmat(x2D, size(x2D'));
+
+figure, imshow(I), title('Señal')
+
+%% Transformada de Fourier 1D
+Xf = fftshift(fft(x1D));
+figure, plot(u, abs(Xf)), grid on
+title('Espectro de la DFT de la señal (en 1D)')
 
 %% Transformada de Fourier 2D
 If = fftshift(fft2(I)); % transformada de I
 
-figure, imshow(mat2gray(abs(If)))
+figure, imshow(mat2gray(abs(If))), title('Espectro de la DFT de la señal (en 2D)')
