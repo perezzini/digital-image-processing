a213cdff703df4178837e4a5d665e17ae001ceca
Luciano Perezzini at Francisco <lperezzini@gmail.com>
2016-12-04 13:12:20 -0300

    TP04/Ejer01a.m: new comments

--- TP04/Ejer01a.m
+++ TP04/Ejer01a.m
@@ -13,7 +13,7 @@ Dt = t(2) - t(1); % discretización de tiempo (Dt es cada cuanto se "muestrea" la
 
 x = cos(2*pi*f0*t); % señal discretizada
 
-X = fftshift(tf1(x)); % transformada de Fourier centrada
+X = fftshift(tf1(x)); % transformada de Fourier centrada ("back-to-back half periods" corregidos)
 
 xi = ift1(ifftshift(X)); % tranformada inversa de Fourier
 xi = real(xi); % nos quedamos sólo con la parte real, porque la parte real es casi cero por aproximaciones de cálculo
@@ -26,7 +26,8 @@ Du = 1/Dt/N; % discretización de frecuencias. Sabemos que la DFT es una
             % período completo de la DFT, para luego antitransformarlo y
             % obtener la función real (continua) original. Por lo tanto
             % hacemos lo siguiente: tomamos sólo N muestras de la DFT en el
-            % intervalo [0, 1/Dt]; por lo tanto, tendremos períodos
+            % intervalo [0, 1/Dt] (en éste intervalo hay "back-to-back 
+            % half periods"); por lo tanto, tendremos "muestreos"
             % iguales de frecuencias Du = (1/Dt)/N.
             
 if rem(N, 2) ~= 0 % N es impar
